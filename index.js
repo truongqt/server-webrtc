aabc4fc9a93914410f40b9f94cb48a08fd471252
Truong <truong@Truongs-MacBook-Pro.local>
2023-03-07 17:13:26 +0700

    add test

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const path = require('path');
-const { createServer } = require('http');
+const path = require("path");
+const {createServer} = require("http");
 
-const express = require('express');
-const { getIO, initIO } = require('./socket');
+const express = require("express");
+const {getIO, initIO} = require("./socket");
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, 'static')));
+app.use("/", express.static(path.join(__dirname, "static")));
 
 const httpServer = createServer(app);
 
 let port = process.env.PORT || 3500;
 
+app.get("/", (req, res) => {
+  res.send("This is index");
+});
+
+app.get("/test", (req, res) => {
+  res.send("TEST ...");
+});
+
 initIO(httpServer);
 
-httpServer.listen(port)
+httpServer.listen(port);
 console.log("Server started on ", port);
 
 getIO();
